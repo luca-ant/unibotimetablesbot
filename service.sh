9ce86e035c7801a35cc5d315a95ba00e8748db27
Luca <luca.ant96@libero.it>
2020-08-29 23:02:50 +0200

    update pip

--- service.sh
+++ service.sh
@@ -12,14 +12,14 @@ OPTION=""
 
 if [ $# == 1 ]
 then
-	OPTION="$1"
+    OPTION="$1"
 fi
 
 
 if [ $OPTION == "run" ]
 then
-	source "$VENV_ACT"
-	python3 "$CURRENT_DIR"/unibotimetablesbot/unibotimetablesbot.py
+    source "$VENV_ACT"
+    python3 "$CURRENT_DIR"/unibotimetablesbot/unibotimetablesbot.py
 
 fi
 
@@ -27,35 +27,36 @@ fi
 if [ $OPTION == "install" ] 
 then
 
-	
-	
-
-	python3 -m venv "$CURRENT_DIR"/venv
-	source "$CURRENT_DIR"/venv/bin/activate
-
-	python3 -m pip install -r "$CURRENT_DIR/"requirements.txt
-	deactivate
 
 
-	BOT_USER=$(whoami)
-	UNI_BOT_TOKEN="YOUR_TOKEN_HERE"
 
-	
-	echo -e "[Unit]
-Description=Unibo timetables bot
-After=network.target
-StartLimitIntervalSec=0
-[Service]
-Environment=UNI_BOT_TOKEN=$UNI_BOT_TOKEN
-WorkingDirectory="$CURRENT_DIR"
-Type=simple
-Restart=always
-RestartSec=1
-User=$BOT_USER
-ExecStart="$CURRENT_DIR/service.sh run"
+    python3 -m venv "$CURRENT_DIR"/venv
+    source "$CURRENT_DIR"/venv/bin/activate
 
-[Install]
-WantedBy=multi-user.target" | sudo tee /etc/systemd/system/unibotimetablesbot.service
+    python3 -m pip install --upgrade pip
+    python3 -m pip install -r "$CURRENT_DIR/"requirements.txt
+    deactivate
+
+
+    BOT_USER=$(whoami)
+    UNI_BOT_TOKEN="YOUR_TOKEN_HERE"
+
+
+    echo -e "[Unit]
+    Description=Unibo timetables bot
+    After=network.target
+    StartLimitIntervalSec=0
+    [Service]
+    Environment=UNI_BOT_TOKEN=$UNI_BOT_TOKEN
+    WorkingDirectory="$CURRENT_DIR"
+    Type=simple
+    Restart=always
+    RestartSec=1
+    User=$BOT_USER
+    ExecStart="$CURRENT_DIR/service.sh run"
+
+    [Install]
+    WantedBy=multi-user.target" | sudo tee /etc/systemd/system/unibotimetablesbot.service
 
 
 fi
