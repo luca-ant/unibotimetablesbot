5b754b567e8c04cf7244e07877177fa9d4f2b047
Luca <luca.ant96@libero.it>
2021-04-06 20:10:54 +0200

    minor changes

--- unibotimetablesbot/config.py
+++ unibotimetablesbot/config.py
@@ -89,7 +89,7 @@ donation_string = emo_money + \
 
 
 location_string = emo_gps+" <b>LOCATION</b>\n\nAre you looking for an empty classroom to study? Try to send your current location and the bot will search empty classrooms around you."
-issue_string = "For issues send a mail to luca.antognetti5@gmail.com describing the problem in detail."
+issue_string = "For issues send a mail to luca.antognetti2@gmail.com describing the problem in detail."
 
 command_help_string = emo_commands+" <b>COMMANDS:</b>\n\n" + SET_NOT_TIME_CMD + " - to set your favourite notification time\n<i>Example:</i> send \"" + \
     SET_NOT_TIME_CMD + \
